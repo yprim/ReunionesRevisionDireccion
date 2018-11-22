9cf71017f338ae26e37999267e33dca00d0d53ab
Priscilla Mena Monge <pmena@lanamme.ucr.ac.cr>
2018-11-22 16:06:24 -0600

    cambios en la logica de datos de hallazgo y sus relaciones

---
+++ Servicios/ReunionElementoRevisarHallazgoServicios.cs
@@ -0,0 +1,14 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class ReunionElementoRevisarHallazgoServicios
+    {
+        ReunionElementoRevisarHallazgoDatos ReunionElementoRevisarHallazgoDatos = new ReunionElementoRevisarHallazgoDatos();
+    }
+}
